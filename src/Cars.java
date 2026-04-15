4b1a1ab7aab2212d38051dc2513c91b780f8f87c
Alex <alex@example.com>
2026-04-15 13:03:23 +0300

    Initial commit

---
+++ src/Cars.java
@@ -0,0 +1,12 @@
+public class Cars {
+   String model;
+
+   public Cars(String model){
+       this.model=model;
+   }
+   public void sayModel() {
+       System.out.println("Masina mea preferata este " +model);
+   }
+
+    }
+
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java World");
+
+        Person y = new Person("Alex");
+        y.sayHello();
+
+        Cars x = new Cars("Nisan");
+        x.sayModel();
+    }
+}
